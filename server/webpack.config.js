39fcc679fc4a8b8138b7099bccc7821f6a44226a
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-03-05 15:04:27 +0600

    Added webpack to build the project and to run bundle as a server instance.

---
+++ server/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpackNodeExternals = require('webpack-node-externals');
+
+const config = {
+  // Inform webpack that we're building a bundle
+  // for Node.js, rather than for the browser
+  target: 'node',
+
+  // Tell webpack the root file of our server application
+  entry: './src/index.js',
+
+  // Tell webpack where to put output file that is generated
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+
+  // Telling webpack not to include any library into the bundle
+  // if this library from /node_modules/ folder
+  externals: [webpackNodeExternals()],
+
+  // Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            ['env', { targets: { browsers: ['last 2 versions'] } }]
+          ],
+          plugins: [
+            ['transform-runtime', {
+              'regenerator': true
+            }]
+          ]
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
